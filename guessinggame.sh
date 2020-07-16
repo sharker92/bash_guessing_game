857cd76fb0d8cb7882a38373245af5de9dc490d4
Andres Morales Reyes <andresmoralesreyes@Andress-MacBook-Pro.local>
2020-07-16 16:12:17 -0600

    Implementing reading just numbers and viewing hidden files

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-COUNT=$(($(ls -l | wc -l)-1))
+RE="^[0-9]+$"
+COUNT=$(($(ls -lA | wc -l)-1))
 echo -e  "Insert the amout of files on this directory: \n$PWD" 
 
 while :
 do
     read TRY
-    if [[ $COUNT -eq $TRY ]]
-    then
-	break
-    elif [[ $TRY -gt $COUNT ]]
-    then
-	echo "Sorry, the number of files is smaller, please try again"
-    elif [[ $TRY -lt $COUNT ]]
-    then
-	echo "Sorry, the number of files is bigger, please try again"
-    fi
-echo "Insert another number:"
+	if [[ $TRY =~ $RE ]]
+	then
+    	if [[ $COUNT -eq $TRY ]]
+    	then
+			break
+    	elif [[ $TRY -gt $COUNT ]]
+    	then
+			echo "Sorry, the number of files is smaller, please try again"
+    	elif [[ $TRY -lt $COUNT ]]
+    	then
+			echo "Sorry, the number of files is bigger, please try again"
+    	fi
+	else
+		echo "Wrong Input!"
+		echo "Please insert a natural number"
+	fi
+echo "Try again, insert a number:"
 done
 echo "Congratulations you enter the correct amount of files!!!"
 
